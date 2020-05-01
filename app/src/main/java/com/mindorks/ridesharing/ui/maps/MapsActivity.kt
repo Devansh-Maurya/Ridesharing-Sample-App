eecb401c058dbcd931ce5ba8657148950c0db4a8
Devansh-Maurya <devansh233@gmail.com>
2020-05-01 20:02:18 +0530

    Added the presenter to the MapsActivity

--- app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/mindorks/ridesharing/ui/maps/MapsActivity.kt
@@ -6,10 +6,16 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.mindorks.ridesharing.R
+import com.mindorks.ridesharing.data.network.NetworkService
 import com.mindorks.ridesharing.utils.ViewUtils
 
 class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
 
+    companion object {
+        private const val TAG = "MapsActivity"
+    }
+
+    private lateinit var presenter: MapsPresenter
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +25,8 @@ class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        presenter = MapsPresenter(NetworkService())
+        presenter.onAttach(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
